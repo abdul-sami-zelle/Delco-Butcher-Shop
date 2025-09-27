68db4a4ea04f68368b3f815c5aad497d7d16a85a
abdul-sami-zelle <abdulsami.zellesolutions@gmail.com>
2025-09-27 21:03:18 +0500

    first

---
+++ src/components/footerBanner/ftBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function FtBanner() {
+    return(
+        <div className="ft_banner">
+            <div className="content_ft_banner">
+                <h2>Butcher</h2>
+                <h3>Shop</h3>
+            </div>
+            <button>Shop Now</button>
+        </div>
+    )
+}
